634f5e6d0c4e69054fe0d1b9c5f94f1fdf2a0dae
Bora-Di <mbou7841@gmail.com>
2024-07-17 13:11:27 +0100

    commit

--- templates_c/e7d7aa0d173e75b09cee8b15e6ababdb95d8ad66_0.file_index.tpl.php
+++ templates_c/e7d7aa0d173e75b09cee8b15e6ababdb95d8ad66_0.file_index.tpl.php
@@ -1,18 +1,18 @@
 <?php
-/* Smarty version 5.3.1, created on 2024-07-17 02:33:53
+/* Smarty version 5.3.1, created on 2024-07-17 13:57:55
   from 'file:templates/index.tpl' */
 
 /* @var \Smarty\Template $_smarty_tpl */
 if ($_smarty_tpl->getCompiled()->isFresh($_smarty_tpl, array (
   'version' => '5.3.1',
-  'unifunc' => 'content_66971171bdcd76_17077873',
+  'unifunc' => 'content_6697b1c3937f11_49420895',
   'has_nocache_code' => false,
   'file_dependency' => 
   array (
     'e7d7aa0d173e75b09cee8b15e6ababdb95d8ad66' => 
     array (
       0 => 'templates/index.tpl',
-      1 => 1721176429,
+      1 => 1721217472,
       2 => 'file',
     ),
   ),
@@ -23,7 +23,7 @@ if ($_smarty_tpl->getCompiled()->isFresh($_smarty_tpl, array (
     'file:includes/footer.tpl' => 1,
   ),
 ))) {
-function content_66971171bdcd76_17077873 (\Smarty\Template $_smarty_tpl) {
+function content_6697b1c3937f11_49420895 (\Smarty\Template $_smarty_tpl) {
 $_smarty_current_dir = 'C:\\xampp\\htdocs\\MediCare_Hospital\\templates';
 $_smarty_tpl->renderSubTemplate("file:includes/header.tpl", $_smarty_tpl->cache_id, $_smarty_tpl->compile_id, 0, $_smarty_tpl->cache_lifetime, array(), (int) 0, $_smarty_current_dir);
 $_smarty_tpl->renderSubTemplate("file:includes/sidebar.tpl", $_smarty_tpl->cache_id, $_smarty_tpl->compile_id, 0, $_smarty_tpl->cache_lifetime, array(), (int) 0, $_smarty_current_dir);
@@ -45,15 +45,15 @@ $_smarty_tpl->renderSubTemplate("file:includes/sidebar.tpl", $_smarty_tpl->cache
                 <div class="col-md-12">
                     <div class="card">
                         <div class="card-header">
-                        <div class="form-inline">
-                        <div class="input-group" data-widget="sidebar-search">
-                          <input class="form-control form-control-sidebar" type="search" placeholder="Search" aria-label="Search">
-                          <div class="input-group-append">
-                            <button class="btn btn-sidebar">
-                              <i class="fas fa-search fa-fw"></i>
-                            </button>
-                          </div>
-                        </div>
+                            <div class="form-inline">
+                                <div class="input-group" data-widget="sidebar-search">
+                                    <input class="form-control form-control-sidebar" type="search" placeholder="Search" aria-label="Search">
+                                    <div class="input-group-append">
+                                        <button class="btn btn-sidebar">
+                                        <i class="fas fa-search fa-fw"></i>
+                                        </button>
+                                    </div>
+                            </div>
                       </div>
                         </div>
                         <div class="card-body">
@@ -93,20 +93,61 @@ $_smarty_tpl->getSmarty()->getRuntime('Foreach')->restore($_smarty_tpl, 1);?>
                             </table>
                         </div>
                     </div>
-                    <div class="card">
-                     <i class="fas fa-search fa-fw"></i>
-                     </div>
                 </div>
             </div>
+             <!-- Info boxes -->
+        <div class="row">
+        <div class="col-12 col-sm-6 col-md-3">
+          <div class="info-box">
+            <span class="info-box-icon bg-info elevation-1"><i class="fas fa-cog"></i></span>
+
+            <div class="info-box-content">
+              <span class="info-box-text">CPU Traffic</span>
+              <span class="info-box-number">
+                10
+                <small>%</small>
+              </span>
+            </div>
+            <!-- /.info-box-content -->
+          </div>
+          <!-- /.info-box -->
         </div>
-        <div class="container-fluid">
-            <div class="row">
-                 <div class="col-md-12">
-                     <div class="card">
-                     <i class="fas fa-search fa-fw"></i>
-                     </div>
-                </div>
+        <!-- /.col -->
+        <div class="col-12 col-sm-6 col-md-3">
+          <div class="info-box mb-3">
+            <span class="info-box-icon bg-danger elevation-1"><i class="fas fa-thumbs-up"></i></span>
+
+            <div class="info-box-content">
+              <span class="info-box-text">Likes</span>
+              <span class="info-box-number">41,410</span>
             </div>
+            <!-- /.info-box-content -->
+          </div>
+          <!-- /.info-box -->
+        </div>
+        <!-- /.col -->
+
+        <!-- fix for small devices only -->
+        <div class="clearfix hidden-md-up"></div>
+
+        <div class="col-12 col-sm-6 col-md-3">
+          <div class="info-box mb-3">
+            <span class="info-box-icon bg-success elevation-1"><i class="fas fa-shopping-cart"></i></span>
+
+            <div class="info-box-content">
+              <span class="info-box-text">Sales</span>
+              <span class="info-box-number">760</span>
+            </div>
+            <!-- /.info-box-content -->
+          </div>
+          <!-- /.info-box -->
+        </div>
+        <!-- /.col -->
+        <!-- /.col -->
+      </div>
+      <!-- /.row -->
+
+            
         </div>
     </section>
 </div>
